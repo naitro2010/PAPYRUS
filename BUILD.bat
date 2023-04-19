adcd06191cfd21d11eb22c9ea50a75bcdbc20ae8
IHateMyKite <globglogabgalab991@gmail.com>
2023-04-19 17:05:05 +0200

    Added auto anonymization of compilated pex scripts

--- BUILD.bat
+++ BUILD.bat
@@ -1,2 +1,20 @@
+@echo off
+
+echo **Running Papyrus Compiler for all script
+
 PapyrusCompiler "../Scripts/Source" -o="../Scripts" -all -i="../Scripts/Source;SRC;SRC_OTHER;SRC_DD;SRC_SKYUI;SRC_UIEXT;SRC_PAPUTIL;SRC_SXL;SRC_MFG;SRC_RM;SRC_FNIS;SRC_SLA;SRC_IWW;SRC_XPMSE;SRC_ST" -optimize -f="Flags.flg"
-if NOT ["%errorlevel%"]==["0"] pause
+
+if NOT ["%errorlevel%"]==["0"] (
+    echo **Error compiling one or more scripts
+pause
+) else (
+    echo **Anonymizing compiled scripts
+    copy .\AFKPexAnon\AFKPexAnon.exe ..\Scripts\AFKPexAnon.exe
+    cd ..\Scripts
+    echo **Running AFKPexAnon
+    AFKPexAnon >NUL
+    del AFKPexAnon.exe
+    echo **Anonymization done
+)
+
+EXIT /B %errorlevel%
--- BUILD_SINGLE.bat
+++ BUILD_SINGLE.bat
@@ -1,3 +1,20 @@
 @echo off
+
+echo **Running Papyrus Compiler for script %1
+
 PapyrusCompiler "../Scripts/Source/"%1 -o="../Scripts" -i="../Scripts/Source;SRC;SRC_OTHER;SRC_DD;SRC_SKYUI;SRC_UIEXT;SRC_PAPUTIL;SRC_SXL;SRC_MFG;SRC_RM;SRC_FNIS;SRC_SLA;SRC_IWW;SRC_XPMSE;SRC_ST" -optimize -f="Flags.flg"
-if NOT ["%errorlevel%"]==["0"] pause
+
+if NOT ["%errorlevel%"]==["0"] (
+    echo **Error compiling script %1
+pause
+) else (
+    echo **Anonymizing compiled scripts
+    copy .\AFKPexAnon\AFKPexAnon.exe ..\Scripts\AFKPexAnon.exe
+    cd ..\Scripts
+    echo **Running AFKPexAnon
+    AFKPexAnon >NUL
+    del AFKPexAnon.exe
+    echo **Anonymization done
+)
+
+EXIT /B %errorlevel%
