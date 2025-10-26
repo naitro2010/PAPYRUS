787e36f9f83d7d9feb456154958f50716dcb0705
naitro2010 <108594252+naitro2010@users.noreply.github.com>
2025-10-26 09:58:28 -0700

    add SkyMessage headers

--- BUILD.bat
+++ BUILD.bat
@@ -2,7 +2,7 @@
 
 echo **Running Papyrus Compiler for all script
 
-PapyrusCompiler "../Scripts/Source" -o="../Scripts" -all -i="../Scripts/Source;SRC_JC;SRC_SKYRIMNET;SRC_SLAL;SRC_SLR;SRC_SSPP;SRC_MME;SRC_UD;SRC;SRC_OTHER;SRC_LAL;SRC_LLOTHER;SRC_ZAZ;SRC_PO3;SRC_DD;SRC_SKYUI;SRC_UIEXT;SRC_PAPUTIL;SRC_SXL;SRC_MFG;SRC_RM;SRC_FNIS;SRC_SLA;SRC_IWW;SRC_XPMSE;SRC_ST" -optimize -f="Flags.flg"
+PapyrusCompiler "../Scripts/Source" -o="../Scripts" -all -i="../Scripts/Source;SRC_JC;SRC_SkyMessage;SRC_SKYRIMNET;SRC_SLAL;SRC_SLR;SRC_SSPP;SRC_MME;SRC_UD;SRC;SRC_OTHER;SRC_LAL;SRC_LLOTHER;SRC_ZAZ;SRC_PO3;SRC_DD;SRC_SKYUI;SRC_UIEXT;SRC_PAPUTIL;SRC_SXL;SRC_MFG;SRC_RM;SRC_FNIS;SRC_SLA;SRC_IWW;SRC_XPMSE;SRC_ST" -optimize -f="Flags.flg"
 
 if NOT ["%errorlevel%"]==["0"] (
     echo **Error compiling one or more scripts
